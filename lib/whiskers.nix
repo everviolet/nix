e4ffe0ac289c228090d40ab90836c97cb510ea88
robin <comfysage@isabelroses.com>
2025-03-07 09:33:31 +0000

    feat(whiskers): pass accents by name

--- lib/whiskers.nix
+++ lib/whiskers.nix
@@ -5,11 +5,30 @@ let
     macchiato = self.evergarden.toCatppuccinPalette self.evergarden.palette.fall;
     frappe = self.evergarden.toCatppuccinPalette self.evergarden.palette.spring;
   };
-  mkWhiskersArgs = flavor: name: value: ''
-    --flavor ${flavor} \
-    --color-overrides '${whiskers-palette}' \
-    --overrides '{ "accent": { "hex": "${value}", "name": "${name}" } }' \
-  '';
+  ctpcolormap =
+    name:
+    {
+      red = "red";
+      orange = "peach";
+      yellow = "yellow";
+      green = "green";
+      aqua = "teal";
+      skye = "sky";
+      blue = "blue";
+      purple = "lavender";
+      pink = "pink";
+    }
+    .${name};
+  mkWhiskersArgs =
+    flavor: name:
+    let
+      value = ctpcolormap name;
+    in
+    ''
+      --flavor ${flavor} \
+      --color-overrides '${whiskers-palette}' \
+      --overrides '{ "accent": { "ctp": "${value}", "name": "${name}" } }' \
+    '';
 in
 {
   inherit whiskers-palette mkWhiskersArgs;
