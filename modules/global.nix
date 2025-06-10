17ae1c0cb946e143b29272f508b72af36bd18ca6
isabel <isabel@isabelroses.com>
2025-06-10 10:28:44 +0100

    feat(global): cache enable

--- modules/global.nix
+++ modules/global.nix
@@ -24,7 +24,21 @@ in
         default = import ../default.nix { inherit pkgs; };
         description = "A map of sources for the evergarden theme";
       };
+
+      cache.enable = lib.mkEnableOption "caching of the evergarden theme";
     };
 
   imports = importApplySelf evgModules;
+
+  config = lib.mkIf config.evergarden.cache.enable {
+    nix.settings = {
+      substituters = [
+        "https://everviolet.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "everviolet.cachix.org-1:3nHvJgzKRRRCQZURheH1INddNlyU4OWqfn068t8AuvU="
+      ];
+    };
+  };
 }
