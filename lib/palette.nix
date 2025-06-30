c8b7914575b6edc2ac7bc381c521f8fa92cdcd1d
robin <comfysage@isabelroses.com>
2025-06-30 17:17:08 +0200

    chore: update palette

--- lib/palette.nix
+++ lib/palette.nix
@@ -4,12 +4,12 @@ rec {
     orange = "F7A182";
     yellow = "F5D098";
     lime = "DBE6AF";
-    green = "CAE0A7";
-    aqua = "ADDEB9";
-    skye = "ACE0D4";
-    snow = "AFDFE6";
-    blue = "B2CFED";
-    purple = "D0BBF0";
+    green = "CBE3B3";
+    aqua = "B3E3CA";
+    skye = "B3E6DB";
+    snow = "AFD9E6";
+    blue = "B2CAED";
+    purple = "D2BDF3";
     pink = "F3C0E5";
     cherry = "F6CEE5";
   };
@@ -22,38 +22,40 @@ rec {
     overlay1 = "6F8788";
     overlay0 = "58686D";
     surface2 = "4A585C";
-    surface1 = "3D494D";
-    surface0 = "313B40";
+    surface1 = "374145";
+    surface0 = "2B3337";
     base = "232A2E";
     mantle = "1C2225";
     crust = "171C1F";
   };
 
   winter = fall // {
-    surface0 = "2D393D";
-    base = "1D2428";
+    surface0 = "262F33";
+    base = "1E2528";
     mantle = "191E21";
   };
 
   spring = fall // {
-    base = "2B3538";
+    surface1 = "3E4A4F";
+    surface0 = "343E43";
+    base = "2B3438";
     mantle = fall.base;
     crust = fall.mantle;
   };
 
   summer = {
     red = "C0696B";
-    orange = "CA8C70";
-    yellow = "CBAE75";
-    lime = "B4C17F";
-    green = "A9BE88";
-    aqua = "91BB9C";
-    skye = "8FBAB5";
-    snow = "8EB4BA";
-    blue = "8CA4BB";
-    purple = "B19AC6";
-    pink = "D0A3C3";
-    cherry = "DBB5CA";
+    orange = "C1866B";
+    yellow = "BC9C6B";
+    lime = "A7AF70";
+    green = "91AC75";
+    aqua = "7BAA92";
+    skye = "79A39B";
+    snow = "7FA0AA";
+    blue = "8CA0BB";
+    purple = "AB9AC6";
+    pink = "CA9EBD";
+    cherry = "CEABBF";
     text = "171C1F";
     subtext1 = "415055";
     subtext0 = "526469";
@@ -63,8 +65,8 @@ rec {
     surface2 = "9CB2B8";
     surface1 = "B4C6CC";
     surface0 = "CED9E0";
-    base = "E9EBF0";
-    mantle = "E1E5ED";
-    crust = "D6DBE9";
+    base = "D7E1EB";
+    mantle = "C8D5E1";
+    crust = "BDCBDB";
   };
 }
