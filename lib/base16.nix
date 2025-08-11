6a3345accef406a9b3fec662779e5501bbfbb036
robin <comfysage@isabelroses.com>
2025-08-12 00:18:14 +0200

    lib/base16: update neutrals

--- lib/base16.nix
+++ lib/base16.nix
@@ -5,22 +5,22 @@ let
   themes = removeAttrs palette [ "colors" ];
 
   mkTheme = theme: {
-    base00 = theme.base;
+    base00 = theme.surface1;
     base01 = theme.red;
     base02 = theme.green;
     base03 = theme.yellow;
     base04 = theme.blue;
     base05 = theme.pink;
     base06 = theme.aqua;
-    base07 = theme.text;
-    base08 = theme.surface0;
+    base07 = theme.subtext0;
+    base08 = theme.overlay1;
     base09 = theme.red;
     base0A = theme.green;
     base0B = theme.yellow;
     base0C = theme.blue;
     base0D = theme.pink;
     base0E = theme.aqua;
-    base0F = theme.subtext0;
+    base0F = theme.subtext1;
   };
 in
 mapAttrs (_: mkTheme) themes
